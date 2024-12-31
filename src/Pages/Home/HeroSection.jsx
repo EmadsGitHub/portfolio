8eaa339ac72ba508adc63c915ccc9b25c30af2b2
EmadsGitHub <143297237+EmadsGitHub@users.noreply.github.com>
2024-12-31 15:31:53 -0500

    Add files via upload

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,16 @@
+export default function  HeroSection(){
+    return (
+        <section id = "heroSection" className = "heroSec">
+            <div className = "hero_section_content_box">
+                <div className = "hero_section_content">
+                    <p className = "section_title">Hey, my name is Emad</p>
+                    <h1 className = "hero_section_title">
+                        <span className = "hero_title_color">Embedded Systems and AI</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
+}
