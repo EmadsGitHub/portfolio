258dd6eeb5d204e41adb68ba45b4de74f1e95c85
EmadsGitHub <143297237+EmadsGitHub@users.noreply.github.com>
2025-01-18 23:37:19 -0500

    Add files via upload

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,26 +1,93 @@
+import { useInView } from 'react-intersection-observer';
+import {useState, useEffect} from 'react';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import {Link} from "react-scroll";
 export default function  HeroSection(){
+
+    const { ref, inView, entry } = useInView({
+        threshold: 0.05, // Change from '0.05' to 0 (number not string)
+        rootMargin: '-50px', // Add some margin to trigger earlier
+        initialInView: true // This ensures it's visible on first load
+    });
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [hasAnimated, setHasAnimated] = useState(false);
+
+    useEffect(()=> {
+        const timer = setTimeout(()=>{
+            setHasAnimated(true);
+            setIsLoaded(true);
+        }, 100);
+        return ()=> {
+            clearTimeout(timer)};
+    }, []);
+
+    useEffect(()=> {
+
+        if (!inView && hasAnimated) {
+            const timer = setTimeout(() => {
+                setHasAnimated(false);
+            }, 300);
+            return () => clearTimeout(timer);
+        }
+        else if (inView && !hasAnimated){
+            setIsLoaded(false);
+            setHasAnimated(true);
+        }
+        
+    }, [inView])
+
+
     return (
         <section id = "heroSection" className = "heroSec">
-            <div className = "hero_section_content_box">
-                <div className = "hero_section_content">
-                    <p className = "section_title">Hey, my name is Emad</p>
-                    <h1 className = "hero_section_title">
-                        <span className = "hero_title_color">Embedded Systems and AI</span>{" "}
-                        <br />
-                        Developer
-                    </h1>
-                    <p className = "hero_section_desc">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        <br/>
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    </p>
+            
 
-                </div>
-                <button className = "button_primary">Contact Me</button>
-            </div>
-            <div className = "img">
-                <img src = "./img/robot.png" alt = "Image"/>
-            </div>
+                    <div className = "hero_section_content_box">
+                        <div ref = {ref} className = {`hero_section_content ${isLoaded||hasAnimated ? 'visible': ''}`}>
+                            <p className = "section_title">Hey, my name is Emad</p>
+                            <h1 className = "hero_section_title">
+                                <span className = "hero_title_color">Embedded Systems and AI</span>{" "}
+                                <br />
+                                Developer
+                            </h1>
+                            <p className = "hero_section_desc">
+                                Who's technical expertise is out of this world!
+                            </p>
+                            <div className = "button_primary">
+                            <Link  activeClass = "navbar_active_content" spy = {true} smooth = {true} offset = {-70} duration = {2000} to = "contactPage" className = "navbar_content"><button>Contact Me</button></Link>
+                            </div>
+                        </div>
+                    
+                    </div>
+                   
+                        
+
+
+                        <div className="moon-container">
+                            <div className="robotimg">
+                                <img src="./img/robotonthemoon.png" alt="Robot on the moon"/>
+                            </div>
+                            
+                            <div className="img">
+                                <img src="./img/crater.png" alt="Moon crater"/>
+                            </div>
+                            
+                            <div className="upsizemoon">
+                                <img src="./img/craterupsidedown.png" alt="Upsize moon"/>
+                            </div>
+                            
+                            <div className="upsidedownimg">
+                                <img src="./img/craterupsidedown.png" alt="Upside down crater"/>
+                            </div>
+                        </div>
+                        
+
+
+            
+            
+                
+
+            
         </section>
     );
 }
---
+++ src/Pages/Home/IconPage.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+const IconPage = () => {
+  return (
+    <div className = "icons">
+
+        <FontAwesomeIcon icon={faArrowRight} />
+      
+    </div>
+  )
+}
+
+export default IconPage
